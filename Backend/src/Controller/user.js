e326b789b5210d4a2883157b78b2dd8b09b9a4c6
Naitik Patel <np03022005@gmail.com>
2024-10-21 12:25:03 +0530

    deployement error solved

--- Backend/src/Controller/user.js
+++ Backend/src/Controller/user.js
@@ -96,6 +96,7 @@ const loginuser = asyncHandler(async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: 'none',
+        domain: 'servicehub-k17j.onrender.com',
     };
     console.log(loggedinuser); 
     return res
